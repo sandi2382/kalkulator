91eec340923ed60572420b64ffebfd267060c422
Sandi Pečečnik <sandi2382@gmail.com>
2019-12-15 14:39:00 +0100

    popravljeno za android worker

--- android/app/src/main/java/com/kalkulator/MainApplication.java
+++ android/app/src/main/java/com/kalkulator/MainApplication.java
@@ -26,7 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return packages;
+          return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new RNThreadPackage(mReactNativeHost)  // <-- Here 
+          );
         }
 
         @Override
