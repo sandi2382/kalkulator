b1522532769222a3420416ab6e964ce9fa9141e8
Sandi Pečečnik <sandi2382@gmail.com>
2019-12-16 11:52:33 +0100

    -krneki

--- android/app/src/main/java/com/kalkulator/MainApplication.java
+++ android/app/src/main/java/com/kalkulator/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-// import com.fabricio.vergal.RNWorkers.RNWorkersPackage;
-import com.reactlibrary.RNThreadPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
